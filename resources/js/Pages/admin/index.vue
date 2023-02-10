4f29a075ac91dcac0ad65b63b9d0491f626c8681
mdmujibur741 <mdmujibur741@gmail.com>
2023-02-10 19:38:26 +0600

    first commit

---
+++ resources/js/Pages/admin/index.vue
@@ -0,0 +1,9 @@
+<template>
+   <AdminLayout>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto animi unde pariatur, possimus maxime saepe, aliquam natus, fugit totam voluptatem ratione. Asperiores nulla ex dolorem, esse exercitationem earum quia enim.
+</AdminLayout>
+</template>
+
+<script setup>
+     import AdminLayout from '../../Layouts/AdminLayout.vue';
+</script>
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+         Auth()->user()->assignRole('admin');
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
+
+Route::get('/admin', function ()
+{
+    return Inertia::render('admin/index');
+});
